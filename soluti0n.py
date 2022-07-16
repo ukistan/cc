a9d0168f99149696d83761d668f1b1315ce97357
ukistan <108941071+ukistan@users.noreply.github.com>
2022-07-17 00:05:23 +0900

    Add files via upload

--- soluti0n.py
+++ soluti0n.py
@@ -1,9 +1,17 @@
-#월 4회 스터디, 1번은 오프라인 3번은 온라인
-#조건1 랜덤으로 날짜를 뽑아야함
-#조건2 월별날짜는 28일까지로 감안
-#조건 3 매월1-3일은 제외
-from random import *
-off1 = randint(4,28)
-off2 = randint(4,28)
-off3 = randint(4,28)
-print ("오프라인 스터디 날짜는 ",off1,",",off2,",",off3,"로 선정되었습니다")
+from re import I
+
+
+def solution(array, commands):
+    answer = []
+    array = list(map(int, input().split()))
+    command = []
+    command[0] = I
+    command[1] = j
+    command[2] = k
+    for com in command:
+        input(com)
+        return 
+     
+
+
+    return answer
