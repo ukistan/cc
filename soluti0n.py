fcf6e472dcb7ea2dd24d7397e1f9f4a2242efbd2
ukistan <108941071+ukistan@users.noreply.github.com>
2022-07-19 00:01:48 +0900

    Add files via upload

--- soluti0n.py
+++ soluti0n.py
@@ -10,11 +10,16 @@ def solution(array, commands):
             answer.append(array[i-1])
             continue
 
-     answer.append(sorted(array[i-1:j])[k-1])
+        answer.append(sorted(array[i-1:j])[k-1])
+     
     
      
 
 
     return answer
+
+    #람다 활용시 커맨즈의 값하나만 x로 활용되는게 아니고 x의 주소값
+    #을 이용해서 커맨즈배열 안 여러 수를 한번의 연산에 활용하는게 신기
+ 
       
     
